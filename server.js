6c6345fdc2f7057c1e946360f6686c8a0a705e1d
Rajpateriya <pateriyaraj52@gmail.com>
2024-06-14 15:52:49 +0530

    proxy configure

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import path from "path";
 import express from "express";
 import  dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 import cookieParser from "cookie-parser";
 
@@ -17,9 +18,15 @@ const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
 
-
-app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  }));
+app.use(express.json());
 app.use(cookieParser());
+
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
